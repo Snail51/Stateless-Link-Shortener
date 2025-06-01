b04d6ce90352eaee4885b4225df172a162ced9bf
mobren11 <mobren11@gmail.com>
2025-06-01 12:14:45 -0500

    integrate "BEST" compression from Snail51/URI-Compressor
    - this commit consumes https://github.com/Snail51/URI-Compressor/commit/6b41af5471c0cbee22f209f022bbd21b9a8aa5a6
    - replace the compression/decompression system from Snail51/URI-Compressor to enable better error handling and "BEST" compression type

---
+++ api.js
@@ -0,0 +1,35 @@
+
+/*
+From: https://github.com/Snail51/URI-Compressor
+Minified with: https://www.toptal.com/developers/javascript-minifier
+Includes code from: https://github.com/nodeca/pako
+Includes code from: https://github.com/LZMA-JS/LZMA-JS
+*/
+
+import PAKO from "./lib/pako-min.js";
+import lzma_init from "./lib/lzma-min.js"; // dont override global "LZMA"; this code natively does that
+
+//This Library
+class URICompressor{static smartPush(e,r="URI-B64",t=8192){let n=[];return["LZMA2","ZLIB"].forEach((s,a)=>{n.push({algorithm:s,result:this.push(e,s,r,t)})}),(n=n.sort((e,r)=>e.result.length-r.result.length))[0].result}static push(e,r="BEST",t="URI-B64",n=8192){if("BEST"===r)return this.smartPush(e,t,n);let s=new TextEncoder;var a=s.encode(e);return a=this.compress(a,r),a=this.encode(a,t),(a=this.buildReturnObject(a,r,t,!0)).length>n&&(a.error="Max length exceeded, data Expunged!",a.data="ERROR"),a}static pull(e,r,t){var n=e,s=r,a=t;try{n=this.decode(n,a),n=this.decompress(n,s);let c=new TextDecoder("utf-8");var n=c.decode(n)}catch(o){n=(n=o.stack).replace(/[\t ]{4,}/g,"	")}return this.buildReturnObject(n,s,a,!1)}static buildReturnObject(e,r,t,n){var s={};return s.length=0,s.isEncoded=n,s.encoding=t,s.length+=t.length,s.compression=r,s.length+=r.length,s.data=e,s.length+=e.length,s}static encode(e,r){return"URI-B64"===r?function e(r){var t="";for(var n of r)t+=String.fromCodePoint(n);var s=btoa(t);return s.replace(/\+/g,"-").replace(/\//g,"_").replace(/\=/g,"")}(e):(console.warn("unrecognized encoding argument: "+r),null)}static compress(e,r){switch(r){case"ZLIB":var t;return t=e,t=PAKO.deflate(t,{level:9});case"LZMA2":return function e(r){var t=[];for(var n of r)t.push(n-128);r=LZMA.compress(t,9);var s=[];for(var n of r)s.push(n+128);return new Uint8Array(s)}(e);default:return console.warn("unrecognized compression argument: "+r),null}}static decode(e,r){return"URI-B64"===r?function e(r){var t=atob(r=r.replace(/\-/g,"+").replace(/\_/g,"/")),n=[];for(var s of t)n.push(s.charCodeAt(0));return new Uint8Array(n)}(e):"Unrecognized encoding argument:	"+r}static decompress(e,r){switch(r){case"ZLIB":return function e(r){try{r=PAKO.inflate(r)}catch(t){r="There was a problem decoding the data in the link.\nAre you sure it was produced by this program?"}return r}(e);case"LZMA2":return function e(r){var t=[];for(var n of r)t.push(n-128);var r=LZMA.decompress(t),s=[];for(var n of r)s.push(n+128);return new Uint8Array(s)}(e);default:return"unrecognized decompression argument:	"+r}}}globalThis.URICompressor=URICompressor;
+
+// Check if a URL parameter was provided
+if (process.argv.length < 6) {
+    console.error('Usage: ./api.js <METHOD> <ENCODING> <COMPRESSION> <DATA>');
+    process.exit(1);
+}
+
+// Extract the components from the command-line arguments
+const method = process.argv[2];
+const encoding = process.argv[3];
+const compression = process.argv[4];
+const data = process.argv[5];
+//console.log(`Method: "${method}", Encoding: "${encoding}", Compression: "${compression}", Data: "${data}"`);
+
+if(method === "ENCODE")
+{
+    console.log(JSON.stringify(globalThis.URICompressor.push(data, compression, encoding, 8192), null, "\t"));
+}
+if(method === "DECODE")
+{
+    console.log(JSON.stringify(globalThis.URICompressor.pull(data, compression, encoding), null, "\t"));
+}
--- unpack.php
+++ unpack.php
@@ -13,65 +13,76 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if(isset($_GET['data']))
+$scriptDir = __DIR__;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
 {
-    $data=$_GET['data'];
+    if(isset($_GET['data']))
+    {
+        $data=$_GET['data'];
+    }
+    else
+    {
+        $data="null";
+    }
+
+    if(isset($_GET['enc']))
+    {
+        $encoding=$_GET['enc'];
+    }
+    else
+    {
+        $encoding="URI-B64";//fallback
+    }
+
+    if(isset($_GET['cmpr']))
+    {
+        $compression=$_GET['cmpr'];
+    }
+    else
+    {
+        $compression="ZLIB";//fallback
+    }
+
+    if($data == "null")
+    {
+        $output = "Error\n\tNo data parameter provided";
+    }
+    else
+    {
+        $cmd = "cd \"{$scriptDir}\" ; node ./api.js \"DECODE\" \"{$encoding}\" \"{$compression}\" \"{$data}\"";
+        $output = shell_exec($cmd);
+        $output = json_decode($output);
+        $output = $output->data;
+    }
+
+    //$output = preg_replace('/[^A-Za-z0-9\n\t\ └├│─​]/', "�", $output);
+
+    $exe_data = explode("\n", $output)[0];
+    $exe_data = preg_replace('/\s*$/', '', $exe_data); //prune whitespace from end
+
+    //impose length limits to avoid breaking previews
+    $exe_title = substr($exe_data, 0, 60) . "...";
+
+    //replace document contents with payloads
+    $content = file_get_contents('./redirect.html');
+    $content = str_replace("{{pageTitle}}", $exe_title, $content);
+    $content = str_replace("{{description}}", "View this shortened link", $content);
+    $content = str_replace("{{tags}}", "Link Shortener,Link,Tool,Free,URL Shortener,URL", $content);
+    $content = str_replace("{{siteName}}", $_SERVER["SERVER_NAME"], $content);
+    $content = str_replace("{{siteURL}}", $_SERVER["SERVER_NAME"], $content);
+    $content = str_replace("{{icon}}", "./link.svg", $content);
+    $content = str_replace("{{redirect}}", $exe_data, $content);
+
+    // return the content (pass the html to the browser for rendering)
+    echo $content;
+
+    exit;
 }
 else
 {
-    $data="null";
+    echo "This API only supports GET requests!";
+
+    exit;
 }
-
-if(isset($_GET['enc']))
-{
-    $encoding=$_GET['enc'];
-}
-else
-{
-    $encoding="URI-B64";//fallback
-}
-
-if(isset($_GET['cmpr']))
-{
-    $compression=$_GET['cmpr'];
-}
-else
-{
-    $compression="ZLIB";//fallback
-}
-
-if($data == "null")
-{
-    $output = "Error\n\tNo data parameter provided";
-}
-else
-{
-    $url = "https://example.com/handler.ext?enc=$encoding&cmpr=$compression&data=$data";
-    $cmd = "node ./decompressor.js \"$url\"";
-    $output = shell_exec($cmd);
-}
-
-//$output = preg_replace('/[^A-Za-z0-9\n\t\ └├│─​]/', "�", $output);
-
-$exe_data = explode("\n", $output)[0];
-$exe_data = preg_replace('/\s*$/', '', $exe_data); //prune whitespace from end
-
-//impose length limits to avoid breaking previews
-$exe_title = substr($exe_data, 0, 60) . "...";
-
-//replace document contents with payloads
-$content = file_get_contents('./redirect.html');
-$content = str_replace("{{pageTitle}}", $exe_title, $content);
-$content = str_replace("{{description}}", "View this shortened link", $content);
-$content = str_replace("{{tags}}", "Link Shortener,Link,Tool,Free,URL Shortener,URL", $content);
-$content = str_replace("{{siteName}}", $_SERVER["SERVER_NAME"], $content);
-$content = str_replace("{{siteURL}}", $_SERVER["SERVER_NAME"], $content);
-$content = str_replace("{{icon}}", "./link.svg", $content);
-$content = str_replace("{{redirect}}", $exe_data, $content);
-
-// return the content (pass the html to the browser for rendering)
-echo $content;
-
-exit; 
-
 ?>
